8846df2c6289723ab33bef14da21b41b1191f724
Jorge Porras <jorgeeporrassandoval@gmail.com>
2026-01-20 10:39:16 -0500

    fix(regional): Se corrige la regional ahora RegionalController maneja la tabla empresa

--- app/Http/Controllers/gestion_regional/RegionalController.php
+++ app/Http/Controllers/gestion_regional/RegionalController.php
@@ -3,29 +3,43 @@
 namespace App\Http\Controllers\gestion_regional;
 
 use App\Http\Controllers\Controller;
-use App\Models\Regional;
+use App\Models\Company;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class RegionalController extends Controller
 {
-    //
     public function store(Request $request)
     {
         try {
             $request->validate([
-                'nombre' => 'required|string|max:255',
-                'telefono' => 'required|string|max:20',
+                'razonSocial' => 'required|string|max:255',
+                'nit' => [
+                    'required',
+                    'string',
+                    'max:255',
+                    Rule::unique('empresa', 'nit'),
+                ],
+                'representanteLegal' => 'required|string|max:20',
                 'direccion' => 'required|string|max:255',
-                'idDepartamento' => 'required|exists:departamento,id'
+                'email' => 'required|string|max:255',
+                'digitoVerificacion' => [
+                    'required',
+                    'integer',
+                    'digits:5',
+                    Rule::unique('empresa', 'digitoVerificacion'),
+                ],
             ]);
 
-            $nuevaRegional = Regional::create([
-                'nombre' => $request->nombre,
-                'telefono' => $request->telefono,
+            $nuevaRegional = Company::create([
+                'razonSocial' => $request->razonSocial,
+                'nit' => $request->nit,
+                'representanteLegal' => $request->representanteLegal,
                 'direccion' => $request->direccion,
-                'idDepartamento' => $request->idDepartamento
+                'email' => $request->email,
+                'rutaLogo' => Company::RUTA_LOGO_DEFAULT,
+                'digitoVerificacion' => $request->digitoVerificacion
             ]);
-
             return response()->json([
                 'status' => 'success',
                 'message' => '¡Regional guardada con éxito!',
@@ -40,9 +54,10 @@ class RegionalController extends Controller
     }
     public function index()
     {
-        $regionales = Regional::with('departamento')->select('id', 'nombre', 'telefono', 'direccion', 'idDepartamento');
+        $regionales = Company::select('id', 'razonSocial', 'nit', 'rutaLogo', 'representanteLegal', 'digitoVerificacion', 'email', 'telefono');
         return response()->json($regionales->get());
     }
+    /**
     public function update(Request $request, $id)
     {
         try {
@@ -70,4 +85,5 @@ class RegionalController extends Controller
             ], 500);
         }
     }
+    */
 }
