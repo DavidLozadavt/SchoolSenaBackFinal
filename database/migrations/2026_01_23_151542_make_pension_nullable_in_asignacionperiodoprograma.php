e4e09ea38fdf5cf7cc872f4f2aada6676e05a3e2
Jorge Porras <jorgeeporrassandoval@gmail.com>
2026-01-23 20:16:45 -0500

    Migración de asignacionperiodoprograma, ahora la pension acepta null y valorPensio tambien

--- database/migrations/2026_01_23_151542_make_pension_nullable_in_asignacionperiodoprograma.php
+++ database/migrations/2026_01_23_151542_make_pension_nullable_in_asignacionperiodoprograma.php
@@ -5,19 +5,20 @@ use Illuminate\Support\Facades\DB;
 
 return new class extends Migration {
     public function up(): void
-{
-    DB::statement("
-        ALTER TABLE asignacionperiodoprograma
-        MODIFY pension TINYINT(1) NULL DEFAULT NULL
-    ");
-}
+    {
+        DB::statement("
+            ALTER TABLE asignacionperiodoprograma
+            MODIFY pension TINYINT(1) NULL DEFAULT NULL,
+            MODIFY valorPension DECIMAL(40,2) NULL DEFAULT NULL
+        ");
+    }
 
-public function down(): void
-{
-    DB::statement("
-        ALTER TABLE asignacionperiodoprograma
-        MODIFY pension TINYINT(1) NOT NULL DEFAULT 1
-    ");
-}
-
+    public function down(): void
+    {
+        DB::statement("
+            ALTER TABLE asignacionperiodoprograma
+            MODIFY pension TINYINT(1) NOT NULL DEFAULT 1,
+            MODIFY valorPension DECIMAL(40,2) NOT NULL DEFAULT 0.00
+        ");
+    }
 };
