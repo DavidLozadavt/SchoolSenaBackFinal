327b38fbe04fd93de6182aa1368745caee9323a4
IngAlim2023 <jorgeeporrassandoval@gmail.com>
2026-02-25 23:49:08 -0500

    Feat(Programas): Programas filtrados por centro de formacion

--- app/Http/Controllers/gestion_programas_academicos/PensumController.php
+++ app/Http/Controllers/gestion_programas_academicos/PensumController.php
@@ -112,12 +112,31 @@ class PensumController extends Controller
         }
     }
 
-    public function indexByRed(int $idRed)
+    public function indexByRed(Request $request, int $idRed)
     {
         try {
+            // Verificamos si se pasó el centro
+            if (!$request->filled('centro')) {
+                return response()->json([
+                    'status' => 'success',
+                    'data' => [] // Retorna vacío si no hay centro
+                ]);
+            }
+
+            $idCentro = $request->centro;
+
             $programas = Programa::with(['nivel', 'tipoFormacion', 'estado', 'red'])
-                ->withCount('fichasActivas')
+                ->withCount([
+                    'fichasActivas as fichas_activas_count' => function ($q) use ($idCentro) {
+                        $q->whereHas('aperturarPrograma.sede', function ($sub) use ($idCentro) {
+                            $sub->where('idCentroFormacion', $idCentro);
+                        });
+                    }
+                ])
                 ->where('idRed', $idRed)
+                ->whereHas('aperturarProgramas.sede', function ($q) use ($idCentro) {
+                    $q->where('idCentroFormacion', $idCentro);
+                })
                 ->orderByDesc('fichas_activas_count')
                 ->orderBy('nombrePrograma')
                 ->get();
