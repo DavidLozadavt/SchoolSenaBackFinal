ab56e3e27852367317a5d23f6ed4a7ea7f5cdb17
Carlos Pinza <carlosdanielpinzadiaz@gmail.com>
2026-03-10 09:22:44 -0500

    feat: agrega gestion de actividades aprendiz y asignacion de participantes

--- routes/api.php
+++ routes/api.php
@@ -132,6 +132,8 @@ use App\Http\Controllers\RedController;
 use App\Http\Controllers\gestion_actividades\ActividadController;
 use App\Http\Controllers\ambiente_virtual\GruposFichaController;
 use App\Http\Controllers\ambiente_virtual\AsignacionActividadController;
+use App\Http\Controllers\ambiente_virtual\ActividadesAprendizController;
+use App\Http\Controllers\ambiente_virtual\CalificacionActividadController;
 use App\Http\Controllers\MatriculaAcademicaController;
 use App\Http\Controllers\AnotacionesDisciplinariasController;
 use App\Http\Controllers\AsistenciaController;
@@ -1270,10 +1272,22 @@ Route::middleware('auth:api')->group(function () {
     Route::get('fichas/{idFicha}/grupos/{id}', [GruposFichaController::class, 'show']);
     Route::put('fichas/{idFicha}/grupos/{id}', [GruposFichaController::class, 'update']);
     Route::delete('fichas/{idFicha}/grupos/{id}', [GruposFichaController::class, 'destroy']);
+    Route::post('fichas/{idFicha}/grupos/{id}/unirse', [GruposFichaController::class, 'unirse']);
+
+    // Actividades del aprendiz (vista estudiante)
+    Route::get('actividades-aprendiz', [ActividadesAprendizController::class, 'index']);
+    Route::post('actividades-aprendiz/entregar', [ActividadesAprendizController::class, 'entregar']);
+
+    // Grupos del estudiante (vista aprendiz)
+    Route::get('grupos-estudiante', [GruposFichaController::class, 'gruposEstudiante']);
 
     // Asignación masiva de actividades (estudiantes/grupos + fecha inicio/fin)
     Route::get('fichas/{idFicha}/asignacion-actividades/datos', [AsignacionActividadController::class, 'datos']);
     Route::post('fichas/{idFicha}/asignacion-actividades', [AsignacionActividadController::class, 'asignar']);
+
+    // Calificación: listar aprendices por actividad y calificar
+    Route::get('actividades/{idActividad}/fichas/{idFicha}/aprendices', [CalificacionActividadController::class, 'listarPorActividad']);
+    Route::post('calificacion-actividad/calificar', [CalificacionActividadController::class, 'calificarIndividual']);
 });//Juicios evaluativos:
 Route::post('raps', action: [TmpRapController::class, 'uploadRaps']);
 
