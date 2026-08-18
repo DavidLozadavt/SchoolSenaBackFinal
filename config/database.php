ba5c2cef96b767ad51e26b5491f65f1fe405fc34
Jeferson Martinez | VΞX <jefermar17jjs@gmail.com>
2026-08-18 18:30:36 -0500

    feat: initialize database configuration with mysql_nexi connection support

--- config/database.php
+++ config/database.php
@@ -104,6 +104,20 @@ return [
             'prefix_indexes' => true,
         ],
 
+        'mysql_nexi' => [
+            'driver' => 'mysql',
+            'host' => env('DB_NEXI_HOST', '127.0.0.1'),
+            'port' => env('DB_NEXI_PORT', '3306'),
+            'database' => env('DB_NEXI_DATABASE', 'vt_inventario'),
+            'username' => env('DB_NEXI_USERNAME', 'root'),
+            'password' => env('DB_NEXI_PASSWORD', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
+
     ],
 
     /*
